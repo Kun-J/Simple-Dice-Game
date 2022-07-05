8889294a033097eb1cd060382fce1b940c5787ad
Kunal Jha <106550721+Kun-J@users.noreply.github.com>
2022-07-05 22:35:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
-
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
-
-var randomImageSource =  randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
 
 var image1 = document.querySelectorAll("img")[0];
 
 image1.setAttribute("src", randomImageSource);
-
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
